d8db47d275256ea1d3dd0e173f950d4eb4f7b9e9
Clément POIRET <poiret.clement@outlook.fr>
2020-03-23 19:36:56 +0100

    Refactor, cleanup and bug fixes

--- app.py
+++ app.py
@@ -14,9 +14,20 @@ from nilearn.image import load_img
 from nilearn.input_data import NiftiMapsMasker
 
 #_path = './data/'
-DEFAULT_KIND = ['correlation', 'partial correlation', 'tangent']
+DEFAULT_KINDS = ['correlation', 'partial correlation', 'tangent']
 DEFAULT_FILTER = '**/*bandpassed*.nii.gz'
 DEFAULT_DOWNLOAD_PATH = './downloaded_atlas.nii.gz'
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 def is_url(s):
@@ -33,6 +44,7 @@ def load_atlas(atlas_location=None, download_path=DEFAULT_DOWNLOAD_PATH):
     Returns:
         {Nibabel Image, list} -- Atlas's path and related labels
     """
+    print(f'{bcolors.OKBLUE}Loading atlas{bcolors.ENDC}')
     atlas_filename = ''
     labels = []
 
@@ -43,7 +55,9 @@ def load_atlas(atlas_location=None, download_path=DEFAULT_DOWNLOAD_PATH):
 
     else:
         if is_url(atlas_location):
-            print('Beginning atlas download with urllib2...')
+            print(
+                f'{bcolors.OKBLUE}Beginning atlas download with urllib2...{bcolors.ENDC}'
+            )
             urllib.request.urlretrieve(atlas_location, download_path)
 
             atlas_filename = download_path
@@ -67,34 +81,85 @@ def load_fmri(path):
     return image
 
 
-def get_correlation_matrix(image,
-                           atlas_filename,
-                           standardize=True,
-                           kind='correlation'):
-    """Computes correlation matrix on a given nibabel image
+def extract_time_series(fmris,
+                        subjects_list,
+                        atlas,
+                        standardize=True,
+                        verbose=5):
+    """Extracting time series from a list of fmris
     
     Arguments:
-        image {Nibabel Image} -- fMRI
-        atlas_filename {str} -- Path to atlas
+        fmris {list} -- List of loaded fMRIs
+        subjects_list {list} -- List of subjects' IDs
+        atlas {str} -- Path to atlas
     
     Keyword Arguments:
-        standardize {bool} -- Standardize (default: {True})
-        kind {str} -- Nilearn's kind of Connectivity Measure (default: {'correlation'})
+        standardize {bool} -- Standardize time series (default: {True})
+        verbose {int} -- Verbosity (default: {5})
     
     Returns:
-        {array} -- Correlation Matrix
+        {tuple} -- Returns time series and processed subjects's IDs
     """
-    masker = NiftiMapsMasker(maps_img=atlas_filename,
-                             standardize=True,
-                             verbose=5)
+    subjects_time_series = []
+    processed_subjects = []
 
-    time_series = masker.fit_transform(image)
+    for fmri in fmris:
+        subject_id = [s for s in subjects_list if s in str(fmri)]
 
-    correlation_measure = ConnectivityMeasure(kind=kind)
-    correlation_matrix = correlation_measure.fit_transform([time_series])[0]
-    z_correlation_matrix = np.arctanh(correlation_matrix)
+        if not subject_id:
+            print(
+                f'{bcolors.WARNING}Found fmri without corresponding ID. Skipping \'{str(fmri)}\'{bcolors.ENDC}'
+            )
+            continue
 
-    return z_correlation_matrix
+        print(f'{bcolors.OKBLUE}Loading {subject_id[0]}{bcolors.ENDC}')
+        processed_subjects.append(subject_id[0])
+
+        img = load_img(fmri.as_posix())
+        masker = NiftiMapsMasker(maps_img=atlas,
+                                 standardize=standardize,
+                                 verbose=verbose)
+        time_series = masker.fit_transform(img)
+
+        subjects_time_series.append(time_series)
+
+    return (subjects_time_series, processed_subjects)
+
+
+def get_connectivity_matrices(time_series, subjects, kinds=DEFAULT_KINDS):
+    """Computes connectivity matrices
+    
+    Arguments:
+        time_series {list} -- List of extracted time series
+        subjects {list} -- List of corresponding subbjects
+    
+    Keyword Arguments:
+        kinds {list} -- List of connectivity measures (default: {DEFAULT_KINDS})
+    
+    Returns:
+        {Numpy array} -- Connectivity matrices
+    """
+    matrices = {}
+
+    for kind in kinds:
+        if kind == 'tangent' and len(set(subjects)) < 2:
+            print(
+                f'{bcolors.FAIL}Tangent space parametrization can only be applied to a group of subjects, as it returns deviations to the mean. Skipping{bcolors.ENDC}'
+            )
+            continue
+
+        print(f'{bcolors.OKBLUE}Computing {kind}{bcolors.ENDC}')
+
+        connectivity_measures = ConnectivityMeasure(kind=kind)
+
+        connectivity_matrices = connectivity_measures.fit_transform(time_series)
+
+        matrices[kind] = {
+            subjects[i]: connectivity_matrices[i]
+            for i in range(connectivity_matrices.shape[0])
+        }
+
+    return matrices
 
 
 def plot(correlation_matrix, labels):
@@ -113,34 +178,15 @@ def plot(correlation_matrix, labels):
     plt.show()
 
 
-def process_fmris(fmris, atlas, kind, subjects):
-    matrices = {}
-
-    subjects_list = pd.read_csv(subjects, header=None)
-    for fmri in fmris:
-        subject_id = [s for s in subjects_list[0] if s in str(fmri)]
-
-        if not subject_id:
-            print('Found fmri without corresponding ID. Skipping \'{}\''.format(
-                str(fmri)))
-            continue
-
-        print('Processing {}'.format(subject_id[0]))
-        fmri = fmri.as_posix()
-
-        loaded_fmri = load_fmri(fmri)
-
-        z_correlation_matrix = get_correlation_matrix(loaded_fmri,
-                                                      atlas,
-                                                      standardize=True,
-                                                      kind=kind)
-
-        matrices[subject_id[0]] = z_correlation_matrix
-
-    return matrices
-
-
 def save_matrices(matrices, path, n_subjects, kinds):
+    """[summary]
+    
+    Arguments:
+        matrices {[type]} -- [description]
+        path {[type]} -- [description]
+        n_subjects {[type]} -- [description]
+        kinds {[type]} -- [description]
+    """
     _filename = '{}_{}subjects_{}.pkl'.format(
         time.time_ns() // 1000000, n_subjects,
         str([kind for kind in kinds
@@ -148,8 +194,12 @@ def save_matrices(matrices, path, n_subjects, kinds):
                                                       '').replace(' ', '-'))
 
     with open(path / _filename, 'wb') as handle:
-        print('Saving matrices to \'{}\'...'.format(path / _filename))
+        print(
+            f'{bcolors.OKBLUE}Saving matrices to \'{path / _filename}\'{bcolors.ENDC}'
+        )
         pickle.dump(matrices, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print(f'{bcolors.OKGREEN}Success!{bcolors.ENDC}')
 
 
 def main(args):
@@ -160,30 +210,37 @@ def main(args):
     subjects = args.subjects
     download_path = args.downloadpath
 
-    print('Loading atlas...')
-    atlas, labels = load_atlas(atlas, download_path=download_path)
+    atlas, _ = load_atlas(atlas, download_path=download_path)
 
     if not _path:
         print(
-            'Argument \'-p <path>\' suggested. Please provide a path containing .nii.gz files. Checking current folder...'
+            f'{bcolors.WARNING}Argument \'-p <path>\' suggested. Please provide a path containing .nii.gz files. Checking current folder{bcolors.ENDC}'
         )
 
     p = Path(_path)
     fmris = sorted(p.glob(_filter))
 
-    if not atlas:
-        print('Loading atlas...')
-        atlas, labels = load_atlas()
+    if not fmris:
+        raise ValueError(
+            f'{bcolors.FAIL}No .nii.gz file found. Please provide a valid path.{bcolors.ENDC}'
+        )
 
-    matrices = {}
-    for kind in kinds:
-        print('Computing {}'.format(kind))
-        matrices[kind] = process_fmris(fmris, atlas, kind, subjects)
+    subjects_list = pd.read_csv(subjects, header=None)[0]
 
-    if not matrices:
-        print('No .nii.gz file found. Please update path or filter.')
-    else:
-        save_matrices(matrices, p, len(matrices[kinds[0]]) + 1, kinds)
+    subjects_time_series, processed_subjects = extract_time_series(
+        fmris=fmris,
+        subjects_list=subjects_list,
+        atlas=atlas,
+        standardize=True,
+        verbose=5)
+
+    matrices = get_connectivity_matrices(
+        time_series=subjects_time_series,
+        subjects=processed_subjects,
+        kinds=kinds,
+    )
+
+    save_matrices(matrices, p, len(matrices[kinds[0]]), kinds)
 
     #plot(correlation_matrix, labels)
 
@@ -213,7 +270,7 @@ if __name__ == "__main__":
         '--kind',
         help=
         'Comma separated list of nilearn\'s kinds (e.g.: "partial correlation,correlation,tangent,covariance,precision"). Default: "correlation,partial correlation"',
-        default=DEFAULT_KIND,
+        default=DEFAULT_KINDS,
         type=str)
     parser.add_argument(
         '-f',
