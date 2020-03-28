679b3bbeed92a1a9c172a37fea46c55f836b27a8
Clément POIRET <poiret.clement@outlook.fr>
2020-03-28 16:51:49 +0100

    Updated default filter

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from nilearn.image import load_img
 from nilearn.input_data import NiftiMapsMasker
 
 DEFAULT_KINDS = ['correlation', 'partial correlation', 'tangent']
-DEFAULT_FILTER = '**/*bandpassed*.nii.gz'
+DEFAULT_FILTER = '**/*.nii.gz'
 DEFAULT_DOWNLOAD_PATH = './downloaded_atlas.nii.gz'
 REGIONS_URL = 'https://raw.githubusercontent.com/clementpoiret/fmri_connectivity_measures/master/files/regions.csv'
 MIST_BASE_URL = 'https://github.com/clementpoiret/fmri_connectivity_measures/raw/master/files/MIST/'
@@ -366,7 +366,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '-f',
         '--filter',
-        help='Regex filter to select fMRI. Default: "**/*bandpassed*.nii.gz"',
+        help='Regex filter to select fMRI. Default: "**/*.nii.gz"',
         default=DEFAULT_FILTER,
         required=False)
     parser.add_argument(
